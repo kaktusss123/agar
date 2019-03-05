679f72fad2f3e21e1f08f552b236342c019ea35e
kaktusss123 <PaRoLoK123>
2019-03-05 18:04:55 +0300

    Шарики ебашатся

--- Ball.py
+++ Ball.py
@@ -1,9 +1,39 @@
+from math import sin
+
+import pygame
+from random import randint
+
+
 class Ball:
 
-    def __init__(self, x, y, r):
-        self.x = x
-        self.y = y
-        self.r = r
+    def __init__(self, radius: int, color: tuple, screen_width: int, screen_height: int):
+        self.x = randint(radius, screen_width - radius)
+        self.y = screen_height + radius
+        self.radius = radius
+        self.color = color
 
-    def move(self, dir):
-        pass
+    def move(self, speed):
+        self.y -= speed
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+
+
+class TexturedBall:
+
+    def __init__(self, texture, screen_width, screen_height, speed):
+        self.size = texture.get_rect()
+        self.x = randint(0, screen_width - self.size[3])
+        self.y = screen_height
+        self.image = texture
+        self.dynamic_x = self.x
+        self.chaos = randint(0, 500)
+        self.speed = randint(1, speed + 1)
+
+    def move(self):
+        self.y -= self.speed
+        self.dynamic_x = self.x + 100 * sin((self.y + self.chaos) / 60)
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.dynamic_x, self.y))
+
--- Game.py
+++ Game.py
@@ -1,25 +1,50 @@
 import pygame
 
+from Ball import Ball, TexturedBall
+
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 600
-CAPTION = 'Eat da balls'
+COLOR_WHITE = 255, 255, 255
+COLOR_BLACK = 0, 0, 0
+CAPTION = 'AGAR'
+MAX_SPEED = 4
+
+COLOR_DARK_BLUE = 0, 34, 56
+
+BALL_COLOR = 255, 66, 82
+BALL_RADIUS = 30
 
 
 class Game:
 
     def __init__(self):
+
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption(CAPTION)
         self.clock = pygame.time.Clock()
 
+        self.balls_list = []
+
         self.run = True
+        self.append = False
 
     def mainloop(self):
         while self.run:
             self.handle_events()
 
-            self.screen.fill((255, 255, 255))
+            self.screen.fill(COLOR_DARK_BLUE)
+
+            # self.test_ball.draw(self.screen)
+            # self.test_ball.move(SPEED)
+            if self.append:
+                self.balls_list.append(
+                    TexturedBall(pygame.image.load('./res/ball_40.png'), SCREEN_WIDTH, SCREEN_HEIGHT, MAX_SPEED))
+
+            for ball in self.balls_list:
+                ball.draw(self.screen)
+                ball.move()
+
             pygame.display.flip()
 
             self.clock.tick(30)
@@ -28,6 +53,12 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == 32:
+                    self.append = True
+            elif event.type == pygame.KEYUP:
+                if event.key == 32:
+                    self.append = False
 
 
 if __name__ == '__main__':
